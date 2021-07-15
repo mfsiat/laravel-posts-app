390f2c24942e79c22fc652139843a4395fa25342
mfsiat <mfsiat>
2021-07-15 20:52:32 +0600

    add routes: registar page

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+      <div class="w-4/12 bg-white p-6 rounded-lg">
+        <form action="" method="post">
+          
+        </form>
+      </div>
+    </div>
+@endsection
