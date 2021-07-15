4bf2f8b9d931bd187c4597b6f2f059e9f219c1d1
mfsiat <mfsiat>
2021-07-15 21:56:41 +0600

    add feture: form validation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,32 +2,90 @@
 
 @section('content')
     <div class="flex justify-center">
-      <div class="w-4/12 bg-white p-6 rounded-lg">
-        <form action="{{ route('register') }}" method="post">
-          <div class="mb-4">
-            <label for="name" class="sr-only">Name</label>
-            <input type="text" name="name" id="name" placeholder="Your name" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
-          </div>
-          <div class="mb-4">
-            <label for="username" class="sr-only">Username</label>
-            <input type="text" name="username" id="username" placeholder="Your username" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
-          </div>
-          <div class="mb-4">
-            <label for="email" class="sr-only">Email</label>
-            <input type="email" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
-          </div>
-          <div class="mb-4">
-            <label for="password" class="sr-only">Password</label>
-            <input type="password" name="password" id="password" placeholder="Your password" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
-          </div>
-          <div class="mb-4">
-            <label for="password" class="sr-only">Password Again</label>
-            <input type="password" name="password_confirmation" id="password" placeholder="Repeat password" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
-          </div>
-          <div>
-            <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Register</button>
-          </div>
-        </form>
-      </div>
+        <div class="w-4/12 bg-white p-6 rounded-lg">
+            <form action="{{ route('register') }}" method="post">
+                @csrf
+                {{-- Name --}}
+                <div class="mb-4">
+                    <label for="name" class="sr-only">Name</label>
+                    <input type="text" name="name" id="name" placeholder="Your name"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="">
+                    {{-- Error handling or Form validation --}}
+                    @error('name')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+
+                {{-- Username --}}
+                <div class="mb-4">
+                    <label for="username" class="sr-only">Username</label>
+                    <input type="text" name="username" id="username" placeholder="Your username"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-500 @enderror"
+                        value="">
+                    {{-- Error handling or Form validation --}}
+                    @error('username')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+
+                {{-- Email --}}
+                <div class="mb-4">
+                    <label for="email" class="sr-only">Email</label>
+                    <input type="email" name="email" id="email" placeholder="Your email"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-500 @enderror"
+                        value="">
+                    {{-- Error handling or Form validation --}}
+                    @error('email')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+
+                {{-- Password --}}
+
+                <div class="mb-4">
+                    <label for="password" class="sr-only">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Your password"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-500 @enderror"
+                        value="">
+                    {{-- Error handling or Form validation --}}
+                    @error('password')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+
+                {{-- Password confirmation --}}
+
+                <div class="mb-4">
+                    <label for="password_confirmation" class="sr-only">Password Again</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation"
+                        placeholder="Repeat password"
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password_confirmation') border-red-500 @enderror"
+                        value="">
+                    {{-- Error handling or Form validation --}}
+                    @error('password_confirmation')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                </div>
+                <div>
+                    <button type="submit"
+                        class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Register</button>
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
