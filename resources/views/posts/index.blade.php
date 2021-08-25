2a865a130eb39c58d9e4d8385e1f68a576f72340
mfsiat <nasirul.islam@outlook.com>
2021-08-25 19:44:12 +0600

    model added for posts individual likes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -32,6 +32,18 @@
                         <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
 
                         <p class="mb-2">{{ $post->body }}</p>
+
+                        <div class="flex items-center">
+                            <form action="" method="POST" class="mr-1">
+                                @csrf
+                                <button type="submit" class="text-blue-500">Like</button>
+                            </form>
+                            <form action="" method="POST" class="mr-1">
+                                @csrf   
+                                <button type="submit" class="text-blue-500">Unlike</button>
+                            </form>
+                        </div>
+
                     </div>
                 @endforeach
                     {{-- This is for the pagination --}}
