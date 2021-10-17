27d2bbbe6e1a70e43dee066f5d6793998cb3923e
Md. Nasirul Islam <nasirul.islam@northsouth.edu>
2021-10-17 23:25:16 +0600

    add single page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,6 +23,14 @@ class PostController extends Controller
         ]);
     }
 
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+
+
     // Store Resources 
     public function store(Request $request)
     {
