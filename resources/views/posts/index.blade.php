5abf3e535c63610f569e6c54c144c21814874ebe
Md. Nasirul Islam <nasirul.islam@northsouth.edu>
2021-10-18 08:51:49 +0600

    excluded post option for unauthorized users

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,25 +5,27 @@
         <div class="w-8/12 bg-white p-6 rounded-lg">
 
             {{-- Post Form --}}
-            <form action="{{ route('posts') }}" method="POST">
-                @csrf
-                <div class="mb-4">
-                    <label for="body" class="sr-only">Body</label>
-                    <textarea name="body" id="body" cols="30" rows="4"
-                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"
-                        placeholder="Post something!"></textarea>
+            @auth
+                <form action="{{ route('posts') }}" method="POST">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="body" class="sr-only">Body</label>
+                        <textarea name="body" id="body" cols="30" rows="4"
+                            class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"
+                            placeholder="Post something!"></textarea>
 
-                    @error('body')
-                        <div class="test-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-                {{-- Post Button --}}
-                <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
-                </div>
-            </form>
+                        @error('body')
+                            <div class="test-red-500 mt-2 text-sm">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    {{-- Post Button --}}
+                    <div>
+                        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+                    </div>
+                </form>
+            @endauth
             @if ($posts->count())
                 @foreach ($posts as $post)
                     <x-post :post="$post" />
